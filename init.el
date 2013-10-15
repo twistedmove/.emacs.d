82566fa21716186e9b4061a94edd727d01669396
Josh Hunsaker <josh.huns@aggiemail.usu.edu>
2013-10-15 06:44:16 -0600

    Major edits to init.el adding many packages back in

--- init.el
+++ init.el
@@ -4,30 +4,44 @@
 (add-to-list 'load-path "~/.emacs.d/site-lisp")
 
 ;; Basic emacs settings
-(global-font-lock-mode 1)	; Enable syntax highlighting
-(global-linum-mode)             ; Enable line numbers
-(setq inhibit-startup-screen t) ; Disable splash screen
-(setq visible-bell t)           ; Disable system beep
-(setq transient-mark-mode t)    ; Enable visual feedback on selections
-(column-number-mode t)		; Show column number on mode line
-(show-paren-mode)		; Show matching parenthesis
+(global-font-lock-mode 1)	        ; Enable syntax highlighting
+(global-linum-mode)                     ; Enable line numbers
+(setq inhibit-startup-screen t)         ; Disable splash screen
+(setq visible-bell t)                   ; Disable system beep
+(setq transient-mark-mode t)            ; Enable visual feedback on selections
+(column-number-mode t)		        ; Show column number on mode line
+(show-paren-mode)		        ; Show matching parenthesis
 
 ;; Disable useless UI features
-(scroll-bar-mode 0)		; Disable scroll bars
-(tool-bar-mode 0)		; Disable toolbar
-(tooltip-mode 0)		; Disable tooltips
-(fringe-mode 0)			; Disable fringes
+(scroll-bar-mode 0)		        ; Disable scroll bars
+(tool-bar-mode 0)		        ; Disable toolbar
+(tooltip-mode 0)		        ; Disable tooltips
+(fringe-mode 0)			        ; Disable fringes
 
 ;; Further customization
-(load-theme 'manoj-dark)	; Set color theme
-(ido-mode t)			; Enable ido mode (interactively do)
-(setq linum-format "%d ")       ; Add space after line numbers
-(setq scroll-step 1)		; Only scroll by one line at top/bottom
-(setq frame-title-format "emacs - %b")
+(load-theme 'manoj-dark)	        ; Set color theme
+(ido-mode t)			        ; Enable ido mode (interactively do)
+(setq linum-format "%d ")               ; Add space after line numbers
+(setq scroll-step 1)		        ; Only scroll by one line at top/bottom
+(setq-default truncate-lines t)         ; Truncate lines by default
+(setq display-time-day-and-date t)      ; Dispaly date along with time in status bar
+(display-time)			        ; Display date and time in status bar
+(setq require-final-newline t)		; Always end a file with a newline
+(setq frame-title-format "emacs - %b")  ; Set frame title to "emacs - <buffer name>"
+
+;; Use unix line endings by default
+(setq default-buffer-file-coding-system 'utf-8-unix)
 
 ;; Comment / Uncomment Lines
 (global-set-key (kbd "C-c c") 'comment-region)
 (global-set-key (kbd "C-c u") 'uncomment-region)
+
+;; Easy buffer swapping
+(require 'buffer-move)
+(global-set-key (kbd "<C-up>")     'buf-move-up)
+(global-set-key (kbd "<C-down>")   'buf-move-down)
+(global-set-key (kbd "<C-left>")   'buf-move-left)
+(global-set-key (kbd "<C-right>")  'buf-move-right)
 
 ;; ! (WINDOWS ONLY) !
 ;; Make sure that the bash executable can be found
@@ -54,10 +68,36 @@
 (global-set-key (kbd "C-c C-SPC") (lambda () (interactive) (mc/create-fake-cursor-at-point)))
 (global-set-key (kbd "<f7>") 'multiple-cursors-mode)
 
+;; Unfortunately, multiple-cursors falls short on rectangular selection
+;; (source: http://www.emacswiki.org/emacs/rect-mark.el)
+(global-set-key (kbd "C-x r C-SPC") 'rm-set-mark)
+(global-set-key (kbd "C-x r C-x") 'rm-exchange-point-and-mark)
+(global-set-key (kbd "C-x r C-w") 'rm-kill-region)
+(global-set-key (kbd "C-x r M-w") 'rm-kill-ring-save)
+(global-set-key (kbd "C-x r C-y") 'yank-rectangle)
+(autoload 'rm-set-mark "rect-mark"
+  "Set mark for rectangle." t)
+(autoload 'rm-exchange-point-and-mark "rect-mark"
+  "Exchange point and mark for rectangle." t)
+(autoload 'rm-kill-region "rect-mark"
+  "Kill a rectangular region and save it in the kill ring." t)
+(autoload 'rm-kill-ring-save "rect-mark"
+  "Copy a rectangular region to the kill ring." t)
+
 ;; Add support for editing matlab files
 ;; (source: http://matlab-emacs.cvs.sourceforge.net/viewvc/matlab-emacs/matlab-emacs/?view=tar)
 (add-to-list 'load-path "~/.emacs.d/site-lisp/matlab-emacs")
 (load-library "matlab-load")
+
+;; Enable column markers at column 81 to warn of long lines
+;; (source: http://www.emacswiki.org/emacs/download/column-marker.el)
+(require 'column-marker)
+(defun marker-at-81 () (interactive) (column-marker-1 81))
+(add-hook 'matlab-mode-hook 'marker-at-81)
+(add-hook 'c-mode-hook 'marker-at-81)
+(add-hook 'c++-mode-hook 'marker-at-81)
+(setq matlab-comment-column 50)
+(setq-default fill-column 81)
 
 ;; Add AUCTeX Mode for generating LaTeX documents
 ;; (source: http://ftp.gnu.org/pub/gnu/auctex/)
@@ -79,6 +119,32 @@
 (add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
 (add-hook 'LaTeX-mode-hook 'TeX-PDF-mode)
 (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+
+;; Add Helm support for advanced searching
+;; (source: https://github.com/emacs-helm/helm.git)
+(add-to-list 'load-path "~/.emacs.d/site-lisp/helm")
+(require 'helm-config)
+
+;; Make helm search open buffers automatically
+;; (source: http://stackoverflow.com/q/14726601)
+(eval-after-load "helm-regexp"
+  '(helm-attrset 'follow 1 helm-source-moccur))
+(defun my-helm-multi-all ()
+  "multi-occur in all buffers backed by files."
+  (interactive)
+  (helm-multi-occur
+   (delq nil
+         (mapcar (lambda (b)
+                   (when (buffer-file-name b) (buffer-name b)))
+                 (buffer-list)))))
+(global-set-key (kbd "S-<f3>") 'my-helm-multi-all)
+
+;; Add sublimity mode for mini-map
+;; (source: https://github.com/zk-phi/sublimity.git)
+(add-to-list 'load-path "~/.emacs.d/site-lisp/sublimity")
+(require 'sublimity)
+(require 'sublimity-scroll)
+(require 'sublimity-map)
 
 ;; Add support for Chrome extension "Edit with Emacs"
 ;; (source: https://github.com/stsquad/emacs_chrome.git)
@@ -115,3 +181,4 @@
  '(column-number-mode t)
  '(show-paren-mode t)
  '(tool-bar-mode nil))
+(put 'dired-find-alternate-file 'disabled nil)
