44fd10681c0bf2085850c01063e7f3b0d9d72b62
Josh Hunsaker <josh.huns@aggiemail.usu.edu>
2013-11-16 14:25:18 -0700

    Updates to get-site-lisp.sh script to make it more robust.

--- get-site-lisp.sh
+++ get-site-lisp.sh
@@ -4,44 +4,34 @@ INSTALL_PATH="${HOME%%/}/.emacs.d/site-lisp"
 [ ! -e "$INSTALL_PATH" ] && mkdir "$INSTALL_PATH"
 cd "$INSTALL_PATH"
 
-REPO="https://github.com/magnars/multiple-cursors.el.git"
-git clone "$REPO" "multiple-cursors" \
-    || { sleep 3 && git clone "$REPO" "multiple-cursors" ; }
+URL="https://github.com/magnars/multiple-cursors.el.git"
+git clone "$URL" "multiple-cursors"
 
-REPO="https://github.com/zk-phi/phi-search.git"
-git clone "$REPO" \
-    || { sleep 3 && git clone "$REPO" ; } \
-    || { sleep 3 && git clone "$REPO" ; }
+git clone "https://github.com/zk-phi/phi-search.git"
+git clone "https://github.com/emacs-helm/helm.git"
+git clone "https://github.com/zk-phi/sublimity.git"
+git clone "https://github.com/stsquad/emacs_chrome.git"
 
-REPO="https://github.com/emacs-helm/helm.git"
-git clone "$REPO" \
-    || { sleep 3 && git clone "$REPO" ; } \
-    || { sleep 3 && git clone "$REPO" ; }
+curl -O "http://www.emacswiki.org/emacs/download/buffer-move.el"
+curl -O "http://www.emacswiki.org/emacs/rect-mark.el"
+curl -O "http://www.emacswiki.org/emacs/download/column-marker.el"
 
-REPO="https://github.com/zk-phi/sublimity.git"
-git clone "$REPO" \
-    || { sleep 3 && git clone "$REPO" ; } \
-    || { sleep 3 && git clone "$REPO" ; }
+URL="http://matlab-emacs.cvs.sourceforge.net/viewvc/matlab-emacs/matlab-emacs/?view=tar"
+NAME="matlab-emacs.tar.gz"
+curl -L "$URL" -o "$NAME" && tar xf "$NAME" && rm "$NAME"
 
-REPO="https://github.com/stsquad/emacs_chrome.git"
-git clone "$REPO" \
-    || { sleep 3 && git clone "$REPO" ; } \
-    || { sleep 3 && git clone "$REPO" ; }
-
-wget "http://www.emacswiki.org/emacs/download/buffer-move.el"
-wget "http://www.emacswiki.org/emacs/rect-mark.el"
-wget "http://www.emacswiki.org/emacs/download/column-marker.el"
-
-wget -O "matlab-emacs.tar.gz" \
-    "http://matlab-emacs.cvs.sourceforge.net/viewvc/matlab-emacs/matlab-emacs/?view=tar" \
-    && tar xf "matlab-emacs.tar.gz" && rm "matlab-emacs.tar.gz"
-
-wget -O "auctex.tar.gz" \
-    "http://ftp.gnu.org/pub/gnu/auctex/auctex-11.87.tar.gz" \
-    && tar xf "auctex.tar.gz" && rm "auctex.tar.gz" \
-    && mv "auctex-11.87" "auctex"
-
-wget -O "dejavu-fonts.tar.bz2" \
-    "http://sourceforge.net/projects/dejavu/files/dejavu/2.34/dejavu-fonts-ttf-2.34.tar.bz2" \
-    && tar xf "dejavu-fonts.tar.bz2" && rm "dejavu-fonts.tar.bz2" \
+NAME="dejavu-fonts.tar.bz2"
+VER="2.34"
+URL="http://sourceforge.net/projects/dejavu/files/dejavu/$VER/dejavu-fonts-ttf-$VER.tar.bz2/download"
+curl -L "$URL" -o "$NAME" \
+    && tar xf "$NAME" && rm "$NAME" \
+    && mv "dejavu-fonts-ttf-$VER" "dejavu-fonts" \
     && echo "-- REMINDER: DejaVu fonts downloaded, but not installed."
+
+NAME="auctex.tar.gz"
+VER="11.87"
+URL="http://ftp.gnu.org/pub/gnu/auctex/auctex-$VER.tar.gz"
+curl -L "$URL" -o "$NAME" \
+    && tar xf "$NAME" && rm "$NAME" \
+    && mv "auctex-$VER" "auctex" && cd "auctex" \
+    &&  echo "-- REMINDER: AUCTeX must be built and installed before use."
