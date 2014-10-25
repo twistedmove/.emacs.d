9bd823eb56782466d325fd9380e8e310f1bfa329
Josh Hunsaker <jph@rincon.com>
2014-10-24 18:03:24 -0700

    Updated my personal helm-config to make it easier to enable follow mode

--- site-lisp/nispio/helm-config.el
+++ site-lisp/nispio/helm-config.el
@@ -1,17 +1,45 @@
-;; Use incremental completion and selection narrowing
+;; Use Helm for incremental completion and selection narrowing
 ;; (source: https://github.com/emacs-helm/helm)
 (use-package helm :ensure t)
 (require 'helm-config)
-
-;; Turn on follow mode when using multi-occur
 (require 'helm-regexp)
-(eval-after-load "helm-regexp"
-  '(setq helm-source-moccur
-	 (helm-make-source "Moccur" 'helm-source-multi-occur :follow 1)))
 
+;; Helm interface for describe bindings
+;; (source: https://github.com/emacs-helm/helm-descbinds)
+(use-package helm-descbinds  :ensure t)
+
+;; Use Helm to choose from a list of Helm commands
+;; (source: https://github.com/vapniks/helm-helm-commands)
+(use-package helm-helm-commands :ensure t)
+
+
+
+(defvar nispio/helm-follow-sources ()
+  "List of sources for which helm-follow-mode should be enabled")
+
+;; Use helm-follow-mode for the following sources:
+(add-to-list 'nispio/helm-follow-sources 'helm-source-occur)
+(add-to-list 'nispio/helm-follow-sources 'helm-source-moccur)
+
+(defun nispio/helm-set-follow ()
+  "Enable helm-follow-mode for the sources specified in the list
+variable `nispio/helm-follow-sources'. This function is meant to
+be run during `helm-initialize' and should be added to the hook
+`helm-before-initialize-hook'."
+  (mapc (lambda (source)
+		  (when (memq source nispio/helm-follow-sources)
+			(helm-attrset 'follow 1 (symbol-value source))))
+		helm-sources))
+
+;; Add hook to enable helm-follow mode for specified helm 
+(add-hook 'helm-before-initialize-hook 'nispio/helm-set-follow)
+
+
+
+;; Do helm-multi-occur in all buffers backed by files
 ;; (source: http://stackoverflow.com/q/14726601)
 (defun nispio/helm-moccur-buffers ()
-  "multi-occur in all buffers backed by files."
+  "helm-multi-occur in all buffers backed by files."
   (interactive)
   (helm-multi-occur
    (delq nil
@@ -19,22 +47,15 @@
 		   (when (buffer-file-name b) (buffer-name b)))
 		 (buffer-list)))))
 
+
 
+;; Expand the Helm window to take up the entire frame
 ;; (source: http://emacs.stackexchange.com/a/650/93)
 (defun nispio/helm-full-frame ()
   (interactive)
   (with-selected-window (helm-window)
     (delete-other-windows)))
 
-
-;; (use-package helm-helm-commands :ensure t)
-;; (require 'helm-helm-commands)
-;; (define-key helm-command-map (kbd "C-8") 'helm-helm-commands)
-
-
-;; Helm interface for describe bindings
-;; (source: https://github.com/emacs-helm/helm-descbinds)
-(use-package helm-descbinds  :ensure t)
-(require 'helm-descbinds)
+
 
 (provide 'nispio/helm-config)
